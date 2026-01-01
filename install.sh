634a163db5c110d1ac82bbbc39d8b2e47285d20a
Yoshiki YAMADA <nyamadandan@gmail.com>
2026-01-01 02:40:43 +0000

    Fix alias for eza command and update install script to use cp instead of ln

--- install.sh
+++ install.sh
@@ -1,26 +1,36 @@
 #! /bin/bash
 set -eux
 
+# get script directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # create directories
 mkdir -p $HOME/.local/bin
 mkdir -p $HOME/.config/nix
 
 # copy config files
-cp -fv "$(pwd)/.gitconfig" "$HOME/.gitconfig"
-cp -fv "$(pwd)/.gitignore" "$HOME/.gitignore"
-cp -fv "$(pwd)/.vimrc" "$HOME/.vimrc"
-cp -fv "$(pwd)/.tmux.conf" "$HOME/.tmux.conf"
-cp -fv "$(pwd)/shell.nix" "$HOME/shell.nix"
-cp -fv "$(pwd)/nix.conf" "$HOME/.config/nix/nix.conf"
-cp -fv "$(pwd)/.bash_tools" "$HOME/.bash_tools"
+cp -fv "$DIR/.gitconfig" "$HOME/.gitconfig"
+cp -fv "$DIR/.gitignore" "$HOME/.gitignore"
+cp -fv "$DIR/.vimrc" "$HOME/.vimrc"
+cp -fv "$DIR/.tmux.conf" "$HOME/.tmux.conf"
+cp -fv "$DIR/shell.nix" "$HOME/shell.nix"
+cp -fv "$DIR/nix.conf" "$HOME/.config/nix/nix.conf"
+cp -fv "$DIR/.bash_tools" "$HOME/.bash_tools"
 
 # install nix-portable
 curl -L https://github.com/DavHau/nix-portable/releases/latest/download/nix-portable-$(uname -m) > $HOME/.local/bin/nix-portable
 chmod +x $HOME/.local/bin/nix-portable
 ln -sfv $HOME/.local/bin/nix-portable $HOME/.local/bin/nix-shell
 
-# .bashrc
+# update .bashrc
+if grep -q "DOTFILES_START" $HOME/.bashrc; then
+    # remove old dotfiles section
+    sed -i '/# DOTFILES_START/,/# DOTFILES_END/d' $HOME/.bashrc
+fi
+
 cat << EOS >> $HOME/.bashrc
+
+# DOTFILES_START
 
 # locale settings
 export LANG=C
@@ -34,5 +44,7 @@ alias dev-shell="NI_PIT=0 nix-shell \$HOME/shell.nix --run 'tmux -u new-session 
 if [ -f \$HOME/.bash_tools ]; then
     . \$HOME/.bash_tools
 fi
+
+# DOTFILES_END
 
 EOS
