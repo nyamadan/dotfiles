0541c4b47911d73081bcbb257d49c59e747464f1
Yoshiki YAMADA <nyamadandan@gmail.com>
2026-08-09 12:33:57 +0900

    fix: prompt install script

--- install.sh
+++ install.sh
@@ -141,8 +141,8 @@ eval "$(zoxide init bash)"
 export EDITOR='nvim'
 
 # PS1
-if [[ -f "\$HOME/.bash_prompt" ]]; then
-    . "\$HOME/.bash_prompt"
+if [[ -f "$HOME/.bash_prompt" ]]; then
+    . "$HOME/.bash_prompt"
 fi
 EOF
 fi
